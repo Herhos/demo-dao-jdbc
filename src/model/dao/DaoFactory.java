ed800bb7a0306f8132c78640d334a79ecfeba45c
Herhos <herhos@outlook.com>
2020-05-24 17:18:33 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+/* A classe DaoFactory vai expor um método que retorna
+ * o tipo da interface (SellerDao), mas internamente vai
+ * instanciar uma implementação  */
+
+public class DaoFactory
+{
+	public static SellerDao createSellerDao()
+	{
+		return new SellerDaoJDBC();
+	}
+}
